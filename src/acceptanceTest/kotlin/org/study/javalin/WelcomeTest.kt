310a5c29a9655ddc343b6f4ae149e9aecd9c942f
Marylly Araújo Silva <marylly.silva@thoughtworks.com>
2019-01-09 18:51:45 -0200

    Create info endpoint json response.

--- src/acceptanceTest/kotlin/org/study/javalin/WelcomeTest.kt
+++ src/acceptanceTest/kotlin/org/study/javalin/WelcomeTest.kt
@@ -4,6 +4,8 @@ import com.github.kittinunf.fuel.Fuel
 import org.assertj.core.api.Assertions.assertThat
 import org.eclipse.jetty.http.HttpStatus
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Assertions.assertNotNull
 import java.nio.charset.Charset
 
 class WelcomeTest {
@@ -15,16 +17,16 @@ class WelcomeTest {
     fun `should receive successfull welcome response on GET to root`() {
         val request = Fuel.get("$baseUrl/")
         val response = request.response().second
-
         assertThat(response.statusCode).isEqualTo(HttpStatus.OK_200)
         assertThat(response.data.toString(Charset.defaultCharset())).isEqualTo("Hello World")
+        assertNotNull(response)
     }
 
     @Test
     fun `should check if exists app info endpoint`() {
         val request = Fuel.get("$baseUrl/info")
         val response = request.response().second
-
         assertThat(response.statusCode).isEqualTo(HttpStatus.OK_200)
+        assertNotNull(response)
     }
 }
--- src/main/kotlin/org/study/javalin/Application.kt
+++ src/main/kotlin/org/study/javalin/Application.kt
@@ -2,19 +2,23 @@ package org.study.javalin
 
 import io.javalin.Javalin
 import io.javalin.apibuilder.ApiBuilder.get
+import io.javalin.json.JavalinJackson
 
 const val DEFAULT_PORT = 7000
 
 class Application {
     val server: Javalin = Javalin.create()
-
+    
     init {
         server.routes { getRoutes() }
     }
 
     private fun getRoutes() {
-        get("/") { context -> context.result("Hello World") }
-        get("info") { }
+        get("/") {  context -> context.result("Hello World") }
+        get("info") {
+            context -> val jsonObject = object {var app = "Hello, World"};
+            context.status(200).json(JavalinJackson.toJson(jsonObject));
+        }
     }
 }
 
